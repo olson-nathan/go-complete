e03d9348a7dcf84f3b1576ca14547327ca2e475b
Nate Olson <nolson@rehearsal.com>
2018-07-16 22:54:25 -0700

    Maps starter code

--- maps/main.go
+++ maps/main.go
@@ -5,30 +5,20 @@ import (
 )
 
 func main() {
-	// Multiple ways to create a map in Go.
-
-	// Literal way.
+	// Create map of [string]string.
 	colors := map[string]string{
 		"red":   "#ff0000",
 		"green": "#fbf745",
+		"white": "#ffffff",
 	}
 
-	// Variable for map so you can add items later.
-	var colors1 map[string]string
-
-	// Another variable declaration that initializes to zero value.
-	colors2 := make(map[string]string)
-
-	// Add key/value to map
-	// NOTE: this can only be added via bracket notation.
-	colors2["white"] = "#ffffff"
-
-	// Delete map key.
-	delete(colors2, "white")
-
 	// Print out mapssss.
-	fmt.Println("Colors literal map::", colors)
-	fmt.Println("Colors map var::", colors1)
-	fmt.Println("Colors make::", colors2)
+	printMap(colors)
 
+}
+
+func printMap(c map[string]string) {
+	for color, hex := range c {
+		fmt.Printf("Color: %v Hex code: %v\n", color, hex)
+	}
 }
