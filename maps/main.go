fbc550b7093318b5ec4acdbc33638622433fcc57
Nate Olson <nolson@rehearsal.com>
2018-07-16 23:00:10 -0700

    Maps completed

--- maps/main.go
+++ maps/main.go
@@ -22,3 +22,7 @@ func printMap(c map[string]string) {
 		fmt.Printf("Color: %v Hex code: %v\n", color, hex)
 	}
 }
+
+// NOTE: Use a map when you are representing closely related values
+// Don't need to know all of the keys at compile time.
+// With stucts you have list out property names at compile time.
